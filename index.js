8c48974778a35613e0074835eed488d9f96d12e3
yhf <xdhfying@gmail.com>
2015-07-23 23:11:09 +0800

    add server log

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var connect = require('connect');
+var log = require('./lib/processor/log');
 var serveStatic = require('serve-static');
 var makeJade = require('./lib/processor/jade');
 var makeLess = require('./lib/processor/less');
+var serveIndex = require('./lib/processor/serveIndex');
 
 module.exports = function(root) {
     return connect()
-        // here add a log lib
+        .use(log(root))
+        .use(serveIndex(root))
         .use(makeJade(root))
         .use(makeLess(root))
         .use(serveStatic(root));
