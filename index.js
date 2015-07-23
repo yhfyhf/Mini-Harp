0ee207a76fe9f2e81e769f7d30c1f4a184592dea
yhf <xdhfying@gmail.com>
2015-07-23 22:46:15 +0800

    add less preprocessing

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
 var makeJade = require('./lib/processor/jade');
+var makeLess = require('./lib/processor/less');
 
 module.exports = function(root) {
     return connect()
         // here add a log lib
         .use(makeJade(root))
+        .use(makeLess(root))
         .use(serveStatic(root));
 };
